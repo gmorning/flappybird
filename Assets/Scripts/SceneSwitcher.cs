1346d65a0b1f418eb3abc90894496cc314e72d46
Dmitry Morozov <dm.gmorning@gmail.com>
2016-08-21 13:43:19 +0300

    two scenes

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{	
+	public void GoToScene(int index)
+	{
+		SceneManager.LoadScene (index);
+	}
+}
