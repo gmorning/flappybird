9ce889221e9562ba5f58e56d0b6537290c85659d
Dmitry Morozov <dm.gmorning@gmail.com>
2016-08-23 23:40:13 +0300

    Movable tubes

---
+++ Assets/Scripts/MoverToTheLeft.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverToTheLeft : MonoBehaviour {
+
+	public float speed;
+	public GameObject prefab;
+	public float heightVariation;
+
+	private bool spawnedNext = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += Vector3.left * speed;
+
+		if (!spawnedNext && transform.position.x < 0) {
+			spawnedNext = true;
+			var tubePair = Instantiate (prefab);
+			tubePair.transform.position = Vector3.right * 4 + Vector3.down * heightVariation + Vector3.up * Random.value * heightVariation * 2;
+		}
+
+		if (transform.position.x < -5)
+			Destroy (gameObject);
+	}
+}
