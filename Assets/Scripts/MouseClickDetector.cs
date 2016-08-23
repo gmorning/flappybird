962e8fc2d8b7d2c769a5c07e6f7b0e94eaede462
Dmitry Morozov <dm.gmorning@gmail.com>
2016-08-23 22:51:57 +0300

    Collisions

---
+++ Assets/Scripts/MouseClickDetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickDetector : MonoBehaviour {
+
+	public float jumpPower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+			rb.velocity = Vector2.zero;
+			rb.AddForce (Vector2.up * jumpPower);
+		}
+	}
+}
